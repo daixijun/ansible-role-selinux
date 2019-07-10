ff0ede07b62d9eb81103bbd0b1e229f821d7d03a
Xijun Dai <daixijun1990@gmail.com>
2019-07-10 15:40:13 +0800

    Fix bare variable

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,12 +2,11 @@ import os
 
 import testinfra.utils.ansible_runner
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts("all")
 
 
 def test_selinux(host):
     cmd = host.run("getenforce")
 
     assert 0 == cmd.rc
-    assert "Disabled" == cmd.stdout
+    assert "Disabled" == cmd.stdout.strip()
